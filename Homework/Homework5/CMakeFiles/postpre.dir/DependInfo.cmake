0997430f37158268df6a5682a4deb8cb6df9a42d
djm214 <djm214@pitt.edu>
2022-02-18 23:51:23 -0500

    getting used to workflow with this homework

---
+++ Homework/Homework5/CMakeFiles/postpre.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dylan/github-test/ECE_302/Homework/Homework5/algebraic_expressions.cpp" "/home/dylan/github-test/ECE_302/Homework/Homework5/CMakeFiles/postpre.dir/algebraic_expressions.cpp.o"
+  "/home/dylan/github-test/ECE_302/Homework/Homework5/postpre.cpp" "/home/dylan/github-test/ECE_302/Homework/Homework5/CMakeFiles/postpre.dir/postpre.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
