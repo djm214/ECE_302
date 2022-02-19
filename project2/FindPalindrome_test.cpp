9d3c2a5fbf43b75d8aeaf4c489e1bf74cffd85d5
djm214 <djm214@pitt.edu>
2022-02-18 23:14:55 -0500

    added a test case for vectors of strings, not just one string value

--- project2/FindPalindrome_test.cpp
+++ project2/FindPalindrome_test.cpp
@@ -28,3 +28,41 @@ TEST_CASE( "add string", "[FindPalindrome]" )
 	REQUIRE(!b.add("Whatz up")); //spaces aren't allowed to be included
 }
 
+TEST_CASE("add vector","[FindPalindrome]")
+{
+	FindPalindrome b; //creates a FindPalindrome object
+
+	std::vector<std::string> stringVector;
+
+	stringVector.push_back("whatsup");
+	stringVector.push_back("hello");
+	stringVector.push_back("string"); //fills a stringVector with various legal words
+	stringVector.push_back("code");
+	stringVector.push_back("naan");
+
+	REQUIRE(stringVector.size() == 5);
+	REQUIRE(b.add(stringVector)); //require that this is true
+	
+	b.clear();
+
+	stringVector.push_back("whatsup");
+	stringVector.push_back("hello");
+	stringVector.push_back("string"); //fills a stringVector with various legal words
+	stringVector.push_back("code");
+	stringVector.push_back("naan");
+	stringVector.push_back("fr3ak"); //add an illegal word
+
+	REQUIRE(!b.add(stringVector)); //require this fails due to the single illegal variable
+
+	b.clear();
+
+	stringVector.push_back("whatsup");
+	stringVector.push_back("hello");
+	stringVector.push_back("very nice"); //add an illegal word
+	stringVector.push_back("string"); //fills a stringVector with various legal words
+	stringVector.push_back("code");
+	stringVector.push_back("naan");
+
+	REQUIRE(!b.add(stringVector)); //require that this fails due to the illegal addition
+	
+}
