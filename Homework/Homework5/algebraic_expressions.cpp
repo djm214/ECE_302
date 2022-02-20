d24e2e70c57c2867957c748a9a1638b4ca46e112
djm214 <djm214@pitt.edu>
2022-02-20 12:59:54 -0500

    done working on the case statements

--- Homework/Homework5/algebraic_expressions.cpp
+++ Homework/Homework5/algebraic_expressions.cpp
@@ -1,5 +1,5 @@
 #include <string>
-using std::string;
+using std::string;   //This code is using a namespace
 
 #include <iostream>
 
@@ -41,8 +41,33 @@ bool isPost(string s) {
   return (firstChar == 0);
 }
 
-void convert(string &postfix, string &prefix) {
+void convert(string &postfix, string &prefix) 
+{
+  string operatorStack;     //creates a stack for the operators found
+  string operandStack;      //creates a stack for the operands
 
-  // TODO
+  for(int i = 0; i < postfix.size(); i++)
+  {
+    switch(postfix.at(i))
+    {
+      case ("+" || "-" || "*" || "/") //case operator
+
+      operatorStack.push_back(); //adds the operator to the operatorStack
+
+      break;
+
+      case("a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"i"||"j"||"k"||"l"||"m"||"n"||"o"
+           "p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z")
+
+        prefix.push_back(); //adds a prefix to the operandStack
+
+      break;
+    }
+  }
+
+  while(operandStack.size()) //while the size of the operand stack is nonzero
+  {
+    
+  }
   
 }
