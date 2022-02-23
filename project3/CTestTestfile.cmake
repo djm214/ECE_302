a8bbaf10419c3e4cc872fe11af71059baf140297
djm214 <djm214@pitt.edu>
2022-02-23 18:05:21 -0500

    buildable files

---
+++ project3/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dylan/github-test/ECE_302/project3
+# Build directory: /home/dylan/github-test/ECE_302/project3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "XMLParser-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/dylan/github-test/ECE_302/project3/CMakeLists.txt;22;add_test;/home/dylan/github-test/ECE_302/project3/CMakeLists.txt;0;")
