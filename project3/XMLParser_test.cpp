6ce95be81998d76b2e40844d84e54ce79656a018
djm214 <djm214@pitt.edu>
2022-03-14 17:50:08 -0400

    added a clear member test

--- project3/XMLParser_test.cpp
+++ project3/XMLParser_test.cpp
@@ -118,3 +118,23 @@ TEST_CASE( "Test Stack size", "[XMLParser]" )
 
 	REQUIRE(intStack.size() == 0); //the stack should now be empty
 }
+
+TEST_CASE( "Test Stack clear", "[XMLParser]" )
+{
+	// Create a Stack to hold ints
+	Stack<int> intStack;
+	int testSize = 5;
+	int stackSize; //holds the stack size
+	bool success; //testing bool
+
+	for (int i=0; i<testSize; i++) 
+	{
+		intStack.push(i); //assuming that the push function works, fill the stack with ints
+		REQUIRE(intStack.size() == (i+1)); //the stack should have a size of 5 after pushing 5 elements onto stack
+	}
+
+	intStack.clear(); //call the clear function
+
+	REQUIRE(intStack.isEmpty()); //ensures that the stack is now empty
+	REQUIRE(intStack.pop() == false);
+}
