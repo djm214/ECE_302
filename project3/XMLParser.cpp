5243a6b46076107a236bb88770d4b34b01db04a1
djm214 <djm214@pitt.edu>
2022-03-15 20:18:56 -0400

    progress

--- project3/XMLParser.cpp
+++ project3/XMLParser.cpp
@@ -24,8 +24,8 @@ bool XMLParser::tokenizeInputString(const std::string &inputString)
 	clear(); //this will ensure that the vector is empty and not tainted from previous runs
 
 	//---------------------------------------------------------------------------------------
-	//Begin Checks/Tests
-	//--------------------------------------------------------------------------------------
+	//--------------------------------Begin Checks/Tests-------------------------------------
+	//---------------------------------------------------------------------------------------
 
 	copyString = inputString; //creates a mutable version of the input string
 	int ammount_of_spaces = 0; //will track the ammount of white spaces
@@ -49,16 +49,133 @@ bool XMLParser::tokenizeInputString(const std::string &inputString)
 		return false; //tokenizing fails if there is only whitespace
 	}
 
-	if(copyString[0] != '<' || copyString[copySting.length() - 1] != '>')
+	if(copyString[0] != '<' || copyString[copyString.length() - 1] != '>')
 	{
 		clear();
 		return false; //if the first and last characters are not '<' and '>' respectively, tokenizing fails
 	}
 
+	//---------------------------------------------------------------------------------------
+	//--------------------------------Begin Tokenization-------------------------------------
+	//---------------------------------------------------------------------------------------
 
+	bool tag = false; //determines if the XML is a tag or not
+	std::string element,content,plain_name; //will hold the strings fro mrkup elements and content and the name of a tag with no attributes
+	TokenStruct to_be_pushed; //a token struct that will be pushed onto the vector
 
-	
+	for(int j = 0; j < copyString.length(); j++)
+	{
+		if(copyString[j] == '<')
+		{
+			element.push_back('<'); //push < onto the string
+			tag = true;
+			continue;
+		}
+		else if(copyString[j] == '>')
+		{
+			element.push_back('>'); //push > onto the string
 
+			int startCount = 0;
+			int endCount = 0;
+
+			for(int k = 0; k < copyString.length(); k++)
+			{
+				if(copyString[k] == '<')
+				{
+					startCount++; //increases the count of < 
+				}
+				if(copyString[k] == '>')
+				{
+					endCount++; //increases the count of >
+				}
+				if(startCount > 1 || endCount > 1)
+				{
+					clear();
+					return false; //nested brackets
+				}
+			}
+
+			switch(element[1])
+			{
+				case '/':
+
+				if(element[element.length() - 2] == '/')
+				{
+					clear(); 
+					return false; //tokenizer fails if </.../> is true, since this is invalid
+				}
+				else
+				{
+					to_be_pushed.tokenType = END_TAG; //if in the form of </...> it is an end tag
+				}
+
+				break;
+
+				case '?':
+
+				if(element[element.length() - 2] != '?')
+				{
+					clear();
+					return false; //if the declaration is missing a ?
+				}
+				else
+				{
+					to_be_pushed.tokenType = DECLARATION; //<?...?> is Declaration form
+				}
+
+				break;
+
+				default:
+
+				if(element[element.length() - 2] == '/')
+				{
+					to_be_pushed.tokenType = EMPTY_TAG; //<.../> is Empty tag form
+				}
+			}
+
+			for(int l = 1; l < element.length(); l++) //this will copy the plain name to a string plain_name
+			{
+				if(element[l] !=  ' ' && element[l] != '>') //as long as the character isn't a space or >, add to the plain_name string
+				{
+					if((l == element.length() - 2) && ((element[l] == '/') || (element[l] == '?'))) 
+					{
+						break; //skip the '/' or '?' if the element is an empty_tag or declaration
+					}
+					if((l == 1) && ((element[l] == '/') || (element[l] == '?'))) 
+					{
+						continue; //exclude the '/' or '?' if the element is a declaration or if the element is an end tag
+					}
+					plain_name.push_back(element[l]);
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			if(plain_name.empty())
+			{
+				clear();
+				return false; //tokenizing fails here by empty name
+			}
+
+			if(plain_name[0] == '0' || plain_name[0] == '1' || plain_name[0] == '2' || plain_name[0] == '3' || plain_name[0] == '4' || plain_name[0] == '5' || plain_name[0] == '6' || plain_name[0] == '7' || plain_name[0] == '8' || plain_name[0] == '9' || plain_name[0] == '-' || plain_name[0] == '.') 
+			{
+				clear();
+				return false; //tokenizing fails here by bad begining character
+			}
+			
+		}
+
+		if(tag)
+		{
+			element.push_back(copySting[j]); //if the item IS a tag
+		}
+		else
+		{
+			content.push_back(copySting[j]); //if the item is content, append to content string
+		}
+	}
 	return true;
 
 }  // end
