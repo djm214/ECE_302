547832759962aec6c11a5bc2f27e615a355c9116
djm214 <djm214@pitt.edu>
2022-04-05 00:08:57 -0400

    in the process of converting pseudo code

--- project4/p4_starter_code/pathfinder.cpp
+++ project4/p4_starter_code/pathfinder.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[])
 {
   Image<Pixel> input = readFromFile(argv[1]); //reads in the image
+  Image<Pixel> output = readFromFile(argv[2]); //what soln wil be written to
   Pixel s; //will hold the starting position
 
   //The following loops will be responsible for locating the starting state
@@ -15,17 +16,46 @@ int main(int argc, char *argv[])
     {
       if(input(i,j) == RED)
       {
-        s = input(i,j);
+        //cases of if the pixel is on the outter edge of the maze already
+        if(i == 0 || i == (input.width()-1))
+        {
+          input(i,j) = GREEN; //write that one pixel to green
+          std::cout<< "Solution found";
+          return EXIT_SUCCESS;
+        }
+        else if(j == 0 || j == (input.height()-1))
+        {
+          input(i,j) = GREEN; //write that one pixel to green
+          std::cout<< "Solution found";
+          return EXIT_SUCCESS;
+        }
+        else
+        {
+          s = input(i,j); //case of solution not equal to goal
+        }
       }
     }
   }
 
-  Deque<Pixel> frontier;
+  Deque<Pixel> frontier; //will hold all non-explored pixels
   frontier.pushBack(s); //push the starting state onto queue
 
   Deque<Pixel> explored; //will keep track of which pixels have been explored
-  
 
+  while(1)
+  {
+    if(frontier.isEmpty())
+    {
+      std::cout << "No Soultion Found";
+      return EXIT_FAILURE;
+    }
+
+    s = frontier.back(); //set s equal to the front of the queue
+    frontier.popBack(); //pop the back of the queue
+
+    explored.pushBack(s); //add s to explored
+
+  }
 
 }
 
