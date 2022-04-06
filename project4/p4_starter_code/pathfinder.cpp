71fa17feb105fa1d483438ac8273bf6c0b8254b8
djm214 <djm214@pitt.edu>
2022-04-05 21:03:47 -0400

    going to ask the TA's tommorow how to keep track of the position of the Pixel

--- project4/p4_starter_code/pathfinder.cpp
+++ project4/p4_starter_code/pathfinder.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-
+#include <vector>
 #include "image.h"
 #include "deque.hpp"
+
+bool isExplored(Pixel,std::vector<Pixel>);
 
 int main(int argc, char *argv[])
 {
   Image<Pixel> input = readFromFile(argv[1]); //reads in the image
   Image<Pixel> output = readFromFile(argv[2]); //what soln wil be written to
   Pixel s; //will hold the starting position
+  int r,c;
 
   //The following loops will be responsible for locating the starting state
   for(int i = 0; i < input.width();i++)
@@ -32,6 +35,8 @@ int main(int argc, char *argv[])
         else
         {
           s = input(i,j); //case of solution not equal to goal
+          r = i;
+          c = j; //will keep track of the position [Pixel at (r,c)]
         }
       }
     }
@@ -40,22 +45,48 @@ int main(int argc, char *argv[])
   Deque<Pixel> frontier; //will hold all non-explored pixels
   frontier.pushBack(s); //push the starting state onto queue
 
-  Deque<Pixel> explored; //will keep track of which pixels have been explored
+  std::vector<Pixel> explored; //will keep track of which pixels have been explored
 
-  while(1)
+  while(!frontier.isEmpty()) //while the fronteir isn't empty
   {
-    if(frontier.isEmpty())
+    s = frontier.front(); //set s equal to the front of the queue
+    frontier.popFront(); //pop the back of the queue
+
+    explored.push_back(s); //add s to explored
+
+    if(input(r-1,c) == WHITE && !isExplored(input(r-1,c),explored))
     {
-      std::cout << "No Soultion Found";
-      return EXIT_FAILURE;
+      frontier.pushBack(input(r-1,c));
+      r--; //update row value
     }
-
-    s = frontier.back(); //set s equal to the front of the queue
-    frontier.popBack(); //pop the back of the queue
-
-    explored.pushBack(s); //add s to explored
-
+    if(input(r+1,c) == WHITE && !isExplored(input(r-1,c),explored))
+    {
+      frontier.pushBack(input(r-1,c));
+      r++; //update row value
+    }
+    if(input(r,c-1) == WHITE && !isExplored(input(r-1,c),explored))
+    {
+      frontier.pushBack(input(r-1,c));
+      c--; //update row value
+    }
+    if(input(r,c+1) == WHITE && !isExplored(input(r-1,c),explored))
+    {
+      frontier.pushBack(input(r-1,c));
+      c++; //update row value
+    }
   }
 
 }
 
+//functions
+bool isExplored(Pixel TBS,std::vector<Pixel> exp)
+{
+  for(int i = 0; i < exp.size(); i++)
+  {
+    if(exp[i] == TBS)
+    {
+      return true; //return true if the pixel has already been explore
+    }
+  }
+  return false; //return false if the pixel has yet to be explored
+}
