36fafe0380e6023db738336f37f2a53e11590a64
djm214 <djm214@pitt.edu>
2022-04-06 09:39:00 -0400

    working?

--- project4/p4_starter_code/pathfinder.cpp
+++ project4/p4_starter_code/pathfinder.cpp
@@ -15,7 +15,6 @@ bool isExplored(Vertex,std::vector<Vertex>);
 int main(int argc, char *argv[])
 {
   Image<Pixel> input = readFromFile(argv[1]); //reads in the image
-  Image<Pixel> output = readFromFile(argv[2]); //what soln wil be written to
   Vertex s; //will hold the starting position
 
   //The following loops will be responsible for locating the starting state
@@ -55,35 +54,76 @@ int main(int argc, char *argv[])
 
   while(!frontier.isEmpty()) //while the fronteir isn't empty
   {
+    std::cout << "top of loop\n";
     s = frontier.front(); //set s equal to the front of the queue
     frontier.popFront(); //pop the back of the queue
 
     int r = s.row;
     int c = s.column;
+    Vertex intermediate; //will be used shortly to push onto queue
+
+    if(r == 0 || c == 0 || r == input.width()-1 || c == input.height()-1)
+    {
+      input(r,c) = GREEN; //color green
+      writeToFile(input,argv[2]);
+      std::cout << "Solution Found";
+      return EXIT_SUCCESS;
+    }
 
     explored.push_back(s); //add s to explored
 
-    if(input(r-1,c) == WHITE && !isExplored(s,explored))
+    if(input(r-1,c) == WHITE)
     {
-      frontier.pushBack(s);
-      r--; //update row value
+      intermediate.p = input(r-1,c); //use intermediate in order to push the different row/column vals
+      intermediate.row = r-1;
+      intermediate.column = c;
+      
+      if(!isExplored(intermediate,explored))
+      {
+        std::cout << "added down\n";
+        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
+      }
     }
-    if(input(r+1,c) == WHITE && !isExplored(s,explored))
+    if(input(r+1,c) == WHITE)
     {
-      frontier.pushBack(s);
-      r++; //update row value
+      intermediate.p = input(r+1,c); //use intermediate in order to push the different row/column vals
+      intermediate.row = r+1;
+      intermediate.column = c;
+      
+      if(!isExplored(intermediate,explored))
+      {
+        std::cout<< "added up\n";
+        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
+      }
     }
-    if(input(r,c-1) == WHITE && !isExplored(s,explored))
+    if(input(r,c-1) == WHITE)
     {
-      frontier.pushBack(s);
-      c--; //update row value
+      intermediate.p = input(r-1,c); //use intermediate in order to push the different row/column vals
+      intermediate.row = r;
+      intermediate.column = c-1;
+      
+      if(!isExplored(intermediate,explored))
+      {
+        std::cout<<"added left\n";
+        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
+      }
     }
-    if(input(r,c+1) == WHITE && !isExplored(s,explored))
+    if(input(r,c+1) == WHITE)
     {
-      frontier.pushBack(s);
-      c++; //update row value
+      intermediate.p = input(r-1,c); //use intermediate in order to push the different row/column vals
+      intermediate.row = r;
+      intermediate.column = c+1;
+
+      if(!isExplored(intermediate,explored))
+      {
+        std::cout << "added right\n";
+        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
+      }
     }
   }
+
+  std::cout << "No Soultion Found"; //if the frotier becomes empty, ther is no soln
+  return EXIT_FAILURE; //exit with failure
 
 }
 
@@ -93,8 +133,11 @@ bool isExplored(Vertex TBS,std::vector<Vertex> exp)
   Vertex tester;
   for(int i = 0; i < exp.size(); i++)
   {
-    tester = exp[i];
-    if((tester.p == TBS.p) && (tester.row == TBS.row) && (tester.column == TBS.column))
+    Vertex tester;
+    tester.p = exp[i].p;
+    tester.row = exp[i].row;
+    tester.column = exp[i].column; //copies all values into a tester vertex
+    if((tester.row == TBS.row) && (tester.column == TBS.column))
     {
       return true; //return true if the pixel has already been explore
     }
