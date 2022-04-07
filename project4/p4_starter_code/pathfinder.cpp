2d3432bd5a76509c05a90f15c056b64f5b8119e0
djm214 <djm214@pitt.edu>
2022-04-07 17:42:08 -0400

    IT WORRKKKKKKSgit add .git add .git add .git add .git add .git add .git add .

--- project4/p4_starter_code/pathfinder.cpp
+++ project4/p4_starter_code/pathfinder.cpp
@@ -5,142 +5,132 @@
 
 struct Vertex
 {
-  Pixel p; //will contain a pixel
   int row;
   int column; //will hold the rows/column values
 };
 
-bool isExplored(Vertex,std::vector<Vertex>);
+Vertex getStartingPosition(Image<Pixel>);
+bool isExplored(int,int,std::vector<Vertex>);
+bool BFS(Vertex,Image<Pixel>&);
 
 int main(int argc, char *argv[])
 {
   Image<Pixel> input = readFromFile(argv[1]); //reads in the image
   Vertex s; //will hold the starting position
+  bool success = false;
 
-  //The following loops will be responsible for locating the starting state
-  for(int i = 0; i < input.width();i++)
+  //hold starting position in s
+  s = getStartingPosition(input);
+
+  //call Breadth-Field Search
+  success = BFS(s,input);
+
+  if(success == false)
   {
-    for(int j = 0; j < input.height();j++)
-    {
-      if(input(i,j) == RED)
-      {
-        //cases of if the pixel is on the outter edge of the maze already
-        if(i == 0 || i == (input.width()-1))
-        {
-          input(i,j) = GREEN; //write that one pixel to green
-          std::cout<< "Solution found";
-          return EXIT_SUCCESS;
-        }
-        else if(j == 0 || j == (input.height()-1))
-        {
-          input(i,j) = GREEN; //write that one pixel to green
-          std::cout<< "Solution found";
-          return EXIT_SUCCESS;
-        }
-        else
-        {
-          s.p = input(i,j); //case of solution not equal to goal
-          s.row = i;
-          s.column = j; //holds coordinates for row/column
-        }
-      }
-    }
+    std::cout << "Solution Not Found";
+    return EXIT_FAILURE;
   }
-
-  Deque<Vertex> frontier; //will hold all non-explored pixels
-  frontier.pushBack(s); //push the starting state onto queue
-
-  std::vector<Vertex> explored; //will keep track of which pixels have been explored
-
-  while(!frontier.isEmpty()) //while the fronteir isn't empty
-  {
-    std::cout << "top of loop\n";
-    s = frontier.front(); //set s equal to the front of the queue
-    frontier.popFront(); //pop the back of the queue
-
-    int r = s.row;
-    int c = s.column;
-    Vertex intermediate; //will be used shortly to push onto queue
-
-    if(r == 0 || c == 0 || r == input.width()-1 || c == input.height()-1)
-    {
-      input(r,c) = GREEN; //color green
-      writeToFile(input,argv[2]);
-      std::cout << "Solution Found";
-      return EXIT_SUCCESS;
-    }
-
-    explored.push_back(s); //add s to explored
-
-    if(input(r-1,c) == WHITE)
-    {
-      intermediate.p = input(r-1,c); //use intermediate in order to push the different row/column vals
-      intermediate.row = r-1;
-      intermediate.column = c;
-      
-      if(!isExplored(intermediate,explored))
-      {
-        std::cout << "added down\n";
-        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
-      }
-    }
-    if(input(r+1,c) == WHITE)
-    {
-      intermediate.p = input(r+1,c); //use intermediate in order to push the different row/column vals
-      intermediate.row = r+1;
-      intermediate.column = c;
-      
-      if(!isExplored(intermediate,explored))
-      {
-        std::cout<< "added up\n";
-        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
-      }
-    }
-    if(input(r,c-1) == WHITE)
-    {
-      intermediate.p = input(r-1,c); //use intermediate in order to push the different row/column vals
-      intermediate.row = r;
-      intermediate.column = c-1;
-      
-      if(!isExplored(intermediate,explored))
-      {
-        std::cout<<"added left\n";
-        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
-      }
-    }
-    if(input(r,c+1) == WHITE)
-    {
-      intermediate.p = input(r-1,c); //use intermediate in order to push the different row/column vals
-      intermediate.row = r;
-      intermediate.column = c+1;
-
-      if(!isExplored(intermediate,explored))
-      {
-        std::cout << "added right\n";
-        frontier.pushBack(intermediate); //if the vertex wasn't explored before, add it to frontier
-      }
-    }
-  }
-
-  std::cout << "No Soultion Found"; //if the frotier becomes empty, ther is no soln
-  return EXIT_FAILURE; //exit with failure
-
+  
+  writeToFile(input,argv[2]);
 }
 
 //functions
-bool isExplored(Vertex TBS,std::vector<Vertex> exp)
+bool isExplored(int row, int column,std::vector<Vertex> exp)
 {
-  Vertex tester;
+  int tester_r,tester_c;
   for(int i = 0; i < exp.size(); i++)
   {
-    Vertex tester;
-    tester.p = exp[i].p;
-    tester.row = exp[i].row;
-    tester.column = exp[i].column; //copies all values into a tester vertex
-    if((tester.row == TBS.row) && (tester.column == TBS.column))
+    tester_r = exp[i].row;
+    tester_c = exp[i].column; //copies all values into a tester vertex
+
+    if((tester_r == row) && (tester_c == column))
     {
-      return true; //return true if the pixel has already been explore
+      return true; //return true if the pixel has already been explored
     }
   }
   return false; //return false if the pixel has yet to be explored
+}
+
+Vertex getStartingPosition(Image<Pixel> image)
+{
+  Vertex start; //starting vertex has i&j rows&columns
+
+  for(int i = 0; i < image.width();i++)
+  {
+    for(int j = 0; j < image.height();j++)
+    {
+      if(image(i,j) == RED)
+      {
+        start.row = i;
+        start.column = j;
+      }
+    }
+  }
+
+  return start;
+}
+
+bool BFS(Vertex start,Image<Pixel>&input)
+{
+  bool explored[input.width()][input.height()]; //will hold all pixels that have been explored
+  
+  Deque<Vertex> frontier;
+  frontier.pushBack(start);
+
+  explored[start.row][start.column] = true;
+
+  Vertex traversal;
+  while(!frontier.isEmpty())
+  {
+    traversal = frontier.front();
+    frontier.popFront();
+
+    int r = traversal.row;
+    int c = traversal.column;
+
+    if(r == 0 || r == input.width()-1 || c == 0 || c == input.height()-1)
+    {
+      input(r,c) = GREEN; //set pixel to green
+      std::cout << "Solution Found";
+      return true;
+    }
+
+    if(input(r-1,c)==WHITE && !explored[r-1][c])
+    {
+      Vertex temp;
+      temp.row = r-1;
+      temp.column = c;
+
+      frontier.pushBack(temp);
+      explored[r-1][c] = true;
+    }
+    if(input(r+1,c)==WHITE && !explored[r+1][c])
+    {
+      Vertex temp;
+      temp.row = r+1;
+      temp.column = c;
+
+      frontier.pushBack(temp);
+      explored[r+1][c] = true;
+    }
+    if(input(r,c-1)==WHITE && !explored[r][c-1])
+    {
+      Vertex temp;
+      temp.row = r;
+      temp.column = c-1;
+
+      frontier.pushBack(temp);
+      explored[r][c-1] = true;
+    }
+    if(input(r,c+1)==WHITE && !explored[r][c+1])
+    {
+      Vertex temp;
+      temp.row = r;
+      temp.column = c+1;
+
+      frontier.pushBack(temp);
+      explored[r][c+1] = true;
+    }
+  }
+  return false;
 }
