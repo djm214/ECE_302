ec16959aa142231129bcd8e5c445f7806600d7ee
djm214 <djm214@pitt.edu>
2022-04-07 23:23:29 -0400

    final submission

--- project4/p4_starter_code/pathfinder.cpp
+++ project4/p4_starter_code/pathfinder.cpp
@@ -9,89 +9,79 @@ struct Vertex
   int column; //will hold the rows/column values
 };
 
-Vertex getStartingPosition(Image<Pixel>);
-bool isExplored(int,int,std::vector<Vertex>);
 bool BFS(Vertex,Image<Pixel>&);
 
 int main(int argc, char *argv[])
 {
   Image<Pixel> input = readFromFile(argv[1]); //reads in the image
-  Vertex s; //will hold the starting position
-  bool success = false;
+  bool success = false; //will determine if BFS failed or succeeded
+  bool startingPixelFound = false; //will be used to tell if a red pixel has been read in
+  Vertex start; //will hold starting vertex
 
-  //hold starting position in s
-  s = getStartingPosition(input);
-
-  //call Breadth-Field Search
-  success = BFS(s,input);
-
-  if(success == false)
+  //scan the png to find a red pixel
+  for(int i = 0; i < input.width();i++)
   {
-    std::cout << "Solution Not Found";
-    return EXIT_FAILURE;
-  }
-  
-  writeToFile(input,argv[2]);
-}
-
-//functions
-bool isExplored(int row, int column,std::vector<Vertex> exp)
-{
-  int tester_r,tester_c;
-  for(int i = 0; i < exp.size(); i++)
-  {
-    tester_r = exp[i].row;
-    tester_c = exp[i].column; //copies all values into a tester vertex
-
-    if((tester_r == row) && (tester_c == column))
+    for(int j = 0; j < input.height();j++)
     {
-      return true; //return true if the pixel has already been explored
-    }
-  }
-  return false; //return false if the pixel has yet to be explored
-}
-
-Vertex getStartingPosition(Image<Pixel> image)
-{
-  Vertex start; //starting vertex has i&j rows&columns
-
-  for(int i = 0; i < image.width();i++)
-  {
-    for(int j = 0; j < image.height();j++)
-    {
-      if(image(i,j) == RED)
+      if(input(i,j) == RED && !startingPixelFound)
       {
-        start.row = i;
+        start.row = i; //store i and j into the start vertex
         start.column = j;
+        startingPixelFound = true; //found one red pixel
+      }
+      else if(input(i,j) == RED && startingPixelFound)
+      {
+        //fails if there is another red pixel
+        std::cout << "There can only be one RED pixel per image\n";
+        return EXIT_FAILURE;
+      }
+
+      if(input(i,j) != RED && input(i,j) != WHITE && input(i,j) != BLACK)
+      {
+        //fails if there is a pixel that is not of red,green,or black
+        std::cout << "Only pixels of color red, green, or black allowed\n";
+        return EXIT_FAILURE;
       }
     }
   }
 
-  return start;
+  //call Breadth-Field Search
+  success = BFS(start,input);
+
+  if(success == false)
+  {
+    //if breadth for Search fails...
+    std::cout << "Solution Not Found\n";
+    return EXIT_FAILURE;
+  }
+  
+  writeToFile(input,argv[2]); //write to file if success == true
 }
 
+//functions
 bool BFS(Vertex start,Image<Pixel>&input)
 {
   bool explored[input.width()][input.height()]; //will hold all pixels that have been explored
   
-  Deque<Vertex> frontier;
-  frontier.pushBack(start);
+  Deque<Vertex> frontier; //a queue to hold pixels that can be explored
+  frontier.pushBack(start); //add the starting vertex to the queue
 
-  explored[start.row][start.column] = true;
+  explored[start.row][start.column] = true; //set it's explored value to true
 
-  Vertex traversal;
+  Vertex traversal; //a vertex that will be used below
   while(!frontier.isEmpty())
   {
-    traversal = frontier.front();
-    frontier.popFront();
+    traversal = frontier.front(); //set traversal to the front of the queue
+    frontier.popFront(); //remove the front of the queue
 
-    int r = traversal.row;
-    int c = traversal.column;
+    int r = traversal.row; 
+    int c = traversal.column; //keep track of the rows/columns in variables r/c
 
+    //check for solution
     if(r == 0 || r == input.width()-1 || c == 0 || c == input.height()-1)
     {
       input(r,c) = GREEN; //set pixel to green
-      std::cout << "Solution Found";
+      std::cout << "Solution Found\n";
       return true;
     }
 
@@ -101,8 +91,8 @@ bool BFS(Vertex start,Image<Pixel>&input)
       temp.row = r-1;
       temp.column = c;
 
-      frontier.pushBack(temp);
-      explored[r-1][c] = true;
+      frontier.pushBack(temp); //pushback new vertex
+      explored[r-1][c] = true; //set it to explored
     }
     if(input(r+1,c)==WHITE && !explored[r+1][c])
     {
@@ -110,8 +100,8 @@ bool BFS(Vertex start,Image<Pixel>&input)
       temp.row = r+1;
       temp.column = c;
 
-      frontier.pushBack(temp);
-      explored[r+1][c] = true;
+      frontier.pushBack(temp); //pushback new vertex
+      explored[r+1][c] = true; //set it to explored
     }
     if(input(r,c-1)==WHITE && !explored[r][c-1])
     {
@@ -119,8 +109,8 @@ bool BFS(Vertex start,Image<Pixel>&input)
       temp.row = r;
       temp.column = c-1;
 
-      frontier.pushBack(temp);
-      explored[r][c-1] = true;
+      frontier.pushBack(temp); //pushback new vertex
+      explored[r][c-1] = true; //set it to explored
     }
     if(input(r,c+1)==WHITE && !explored[r][c+1])
     {
@@ -128,9 +118,9 @@ bool BFS(Vertex start,Image<Pixel>&input)
       temp.row = r;
       temp.column = c+1;
 
-      frontier.pushBack(temp);
-      explored[r][c+1] = true;
+      frontier.pushBack(temp); //pushback new vertex
+      explored[r][c+1] = true; //set it to explored
     }
   }
-  return false;
+  return false; //return false if the function makes it here
 }
